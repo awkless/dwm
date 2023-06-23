6e573ea4f819eb188bb31c4ac721e622e6ef08dd
Jason Pena <jasonpena@awkless.com>
2023-06-23 11:27:14 -0600

    fix: Fix styling of passmenu2
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- config.h
+++ config.h
@@ -80,7 +80,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *passmenu[] = { "passmenu2", "-m", dmenumon, NULL };
+static const char *passmenu[] = { "passmenu2", "-m", dmenumon, "-g", "1", "-l", "5", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 /* Commands to manipulate volume... */
